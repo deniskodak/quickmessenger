ff9f62c2f4ec44f4757f31051588be1577fe11ef
Denys Kodak <dkodak@deloitte.ua>
2023-01-15 14:45:27 +0200

    Messegner: commit mongo db connection

--- server/bin/server.js
+++ server/bin/server.js
@@ -3,16 +3,20 @@ const dbo = require('../db')
 const app = require("../app.js");
 const { PORT = 3000 } = process.env;
 
-dbo.connectToServer(function (err) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+// dbo.connectToServer(function (err) {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
 
-  // start the Express server
-  app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-  });
+//   // start the Express server
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
 
 module.exports = app;
