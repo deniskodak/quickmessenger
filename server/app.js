f1a12f42ea1365f1c7a4f920eb5dc4568496ecfc
Denys Kodak <dkodak@deloitte.ua>
2023-01-17 23:14:21 +0200

    Messenger: add headers to response

--- server/app.js
+++ server/app.js
@@ -16,10 +16,13 @@ const router = require("./routes/index");
 
 app.use("/v1/openAi", router);
 app.use("/v1/message", (req, res) => {
+  res.header("Access-Control-Allow-Origin", '*')
+  res.header("Access-Control-Allow-Methods", 'GET, HEAD, OPTIONS, POST, PUT, DELETE')
+  res.header("Access-Control-Allow-Headers", 'Origin, Content-type, Accept, Authorization, X-Requested-With')
+
   res.status(200).json({
     message: 'hello world'
   })
-
 })
 app.use((_, res) => {
   res.status(404).json({ status: "error", code: 404, message: "Not found" });
